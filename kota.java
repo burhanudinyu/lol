e67d0ae86d04bbd6a7e3a6de4be6ea7c5fd0d629
burhanudinyu <burhansx@gmail.com>
2015-04-03 09:24:51 +0700

    Create kota.java

---
+++ kota.java
@@ -0,0 +1,33 @@
+public class kota {
+String kota;
+int x,y;
+
+    public kota(String kota) {
+        this.kota = kota;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
